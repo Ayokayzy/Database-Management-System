e8b110de336089dc078fea5a07719f69f6a60743
Ayokayzy <theayokayzy1@gmail.com>
2023-10-13 09:02:35 +0100

    update

--- src/screens/home.jsx
+++ src/screens/home.jsx
@@ -1,14 +1,4 @@
 import React, { useState } from "react";
-import Background from "../components/mackground/background.component";
-
-// background images
-import bg1 from "../assets/bg-1.1.png";
-import bg2 from "../assets/bg-2.png";
-import { motion, AnimatePresence } from "framer-motion";
-import Sound from "../components/sound/sound.component";
-import Back from "../components/back/back.component";
-import DisplayBoard from "../components/display-board/display-board.component";
-import OptionButton from "../components/option-button/option-button.component";
 
 const Home = () => {
   return (
